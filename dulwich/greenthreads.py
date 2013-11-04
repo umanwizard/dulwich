4bc8c3b46ed8d41cfddc863d9b545c2c70116b9b
Jelmer Vernooij <jelmer@samba.org>
2013-11-04 10:11:35 +0000

    Update NEWS, add docstrings.

--- dulwich/greenthreads.py
+++ dulwich/greenthreads.py
@@ -19,6 +19,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA  02110-1301, USA.
 
+"""Utility module for querying an ObjectStore with eventlet."""
+
 import eventlet
 
 from dulwich.objects import (
